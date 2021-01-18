3bcce8588b652cec17364cb7eb1e530466a58520
kirillbelyaev74 <kirbel74@gmail.com>
2021-01-18 13:35:34 +0500

    3.1. Прерывание блокированной нити.[267413#450015]

---
+++ chapter_001/src/main/java/ru/job4j/concurrent/ThreadStop.java
@@ -0,0 +1,21 @@
+package ru.job4j.concurrent;
+
+public class ThreadStop {
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(() -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println("start ...");
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        progress.start();
+        Thread.sleep(1000);
+        progress.interrupt();
+        progress.join();
+    }
+}
