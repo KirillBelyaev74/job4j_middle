ca30c82ad3dbb0606c4ffb36af5d377457db5eda
kirillbelyaev74 <kirbel74@gmail.com>
2021-01-18 14:34:57 +0500

    4. Скачивание файла с ограничением.[144271#450012]

---
+++ chapter_001/src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,49 @@
+package ru.job4j.concurrent;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new URL(this.url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            long timeStart = System.currentTimeMillis();
+            while ((bytesRead = bufferedInputStream.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                long timeFinish = System.currentTimeMillis();
+                int result = (int) (timeFinish - timeStart);
+                if (result < this.speed) {
+                    try {
+                        Thread.sleep(this.speed - result);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
+        int speed = 1000;
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+}
