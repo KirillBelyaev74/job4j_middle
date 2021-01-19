7075e9404915a3138bab51ce85dc6fb294b721ce
kirillbelyaev74 <kirbel74@gmail.com>
2021-01-19 22:26:09 +0500

    1.1. Состояние нити.[229175#450013]

--- chapter_001/src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ chapter_001/src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -13,16 +13,12 @@ public class ConcurrentOutput {
         second.start();
         first.start();
 
-        try {
-            second.join();
-            first.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        if (first.getState() == Thread.State.TERMINATED && second.getState() == Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getState());
             System.out.println(second.getState());
         }
+
+        System.out.println(first.getState());
+        System.out.println(second.getState());
     }
 }
