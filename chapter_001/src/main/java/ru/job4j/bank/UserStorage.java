622d031bf6a8a83a638a65dba7b58cd76619a44c
kirillbelyaev74 <kirbel74@gmail.com>
2021-01-22 18:20:42 +0500

    3. Класс хранилища пользователей UserStorage[1104#450028]

--- chapter_001/src/main/java/ru/job4j/bank/UserStorage.java
+++ chapter_001/src/main/java/ru/job4j/bank/UserStorage.java
@@ -4,80 +4,53 @@ import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ThreadSafe
 public class UserStorage {
 
     @GuardedBy("this")
-    private final List<User> list = new ArrayList<>();
+    private final Map<Integer, User> map = new HashMap<>();
 
     public synchronized boolean add(User user) {
-        if (user == null) {
-            throw new NullPointerException();
-        }
-        return this.list.add(user);
-
-    }
-
-    public synchronized boolean update(User user) {
-        if (user == null) {
-            throw new NullPointerException();
-        }
         boolean result = false;
-        for (int index = 0; index != this.list.size(); index++) {
-            if (user.getId() == this.list.get(index).getId()) {
-                this.list.set(index, user);
-                result = true;
-                break;
-            }
-        }
-
-        return result;
-    }
-
-    public synchronized boolean delete(User user) {
-        if (user == null) {
-            throw new NullPointerException();
-        }
-        return this.list.remove(user);
-    }
-
-    public synchronized boolean transfer(int fromId, int toId, int amount) {
-        int fromIndex = this.findIndexID(fromId);
-        int toIndex = this.findIndexID(toId);
-        boolean result = false;
-        if (fromIndex != -1 && toIndex != -1) {
-            User fromUser = this.list.get(fromIndex);
-            User toUser = this.list.get(toIndex);
-            this.list.set(fromIndex, new User(fromUser.getId(), fromUser.getAmount() - amount));
-            this.list.set(toIndex, new User(toUser.getId(), toUser.getAmount() + amount));
+        if (user != null) {
+            this.map.putIfAbsent(user.getId(), user);
             result = true;
         }
         return result;
     }
 
-    private synchronized int findIndexID(int id) {
-        int result = -1;
-        for (int index = 0; index != this.list.size(); index++) {
-            if (this.list.get(index).getId() == id) {
-                result = index;
-            }
+    public synchronized boolean update(User user) {
+        if (user != null) {
+            this.map.replace(user.getId(), user);
+        }
+        return this.map.containsValue(user);
+    }
+
+    public synchronized boolean delete(User user) {
+        return this.map.remove(user.getId(), user);
+    }
+
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        User fromUser = this.map.get(fromId);
+        User toUser = this.map.get(toId);
+        boolean result = false;
+        if (fromUser != null && toUser != null) {
+            this.map.replace(fromId, new User(fromId, fromUser.getAmount() - amount));
+            this.map.replace(toId, new User(toId, toUser.getAmount() - amount));
+            result = true;
         }
         return result;
     }
 
     public synchronized User findUserID(int id) {
-        User user = null;
-        for (int index = 0; index != this.list.size(); index++) {
-            if (this.list.get(index).getId() == id) {
-                user = this.list.get(index);
-            }
-        }
-        return user;
+        return this.map.get(id);
     }
 
     public synchronized int getCountUser() {
-        return this.list.size();
+        return this.map.size();
     }
 }
