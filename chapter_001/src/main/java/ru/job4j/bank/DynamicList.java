3c95214f301c74dda330dbf50b97f4ed08948a83
kirillbelyaev74 <kirbel74@gmail.com>
2021-01-25 11:26:36 +0500

    4. ThreadSafe динамический список [1105#450029]

---
+++ chapter_001/src/main/java/ru/job4j/bank/DynamicList.java
@@ -0,0 +1,60 @@
+package ru.job4j.bank;
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class DynamicList<E> implements Iterable<E> {
+
+    private Object[] container;
+    private int position = 0;
+
+    public DynamicList() {
+        this.container = new Object[10];
+    }
+
+    public void add(E value) {
+        if (this.container.length == this.position + 1) {
+            this.container = Arrays.copyOf(this.container, this.container.length + 1);
+        }
+        this.container[position] = value;
+        position++;
+    }
+
+    public E get(int index) throws ArrayIndexOutOfBoundsException {
+        if (index < 0 || index > this.container.length) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return (E) this.container[index];
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+
+            int modCount = position;
+
+            int itPosition = 0;
+
+            @Override
+            public boolean hasNext() throws ConcurrentModificationException {
+                boolean result = false;
+                if (itPosition != position) {
+                    result = true;
+                }
+                if (modCount != position) {
+                    throw new ConcurrentModificationException();
+                }
+                return result;
+            }
+
+            @Override
+            public E next() throws NoSuchElementException {
+                if (hasNext()) {
+                    return (E) container[itPosition++];
+                }
+                throw new NoSuchElementException();
+            }
+        };
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/bank/SingleLockList.java
@@ -0,0 +1,36 @@
+package ru.job4j.bank;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.Iterator;
+
+@ThreadSafe
+public class SingleLockList<T> implements Iterable<T> {
+
+    private final DynamicList<T> dynamicList;
+
+    public SingleLockList() {
+        this.dynamicList = new DynamicList<>();
+    }
+
+    public synchronized void add(T value) {
+        this.dynamicList.add(value);
+    }
+
+    public synchronized T get(int index) {
+        return this.dynamicList.get(index);
+    }
+
+    @Override
+    public synchronized Iterator<T> iterator() {
+        return this.copy(this.dynamicList).iterator();
+    }
+
+    public DynamicList<T> copy(DynamicList<T> list) {
+        DynamicList<T> result = new DynamicList<>();
+        for (T t : this.dynamicList) {
+            result.add(t);
+        }
+        return result;
+    }
+}
